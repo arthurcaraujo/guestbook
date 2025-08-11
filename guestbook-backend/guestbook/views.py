135eb8b43d3e8712c65c6e1da89d0214ebcfc236
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-08-11 20:00:25 -0300

    feat: Create view to list and add guestbook messages

--- guestbook-backend/guestbook/views.py
+++ guestbook-backend/guestbook/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Message
+from .serializers import MessageSerializer
 
-# Create your views here.
+class MessageListCreateView(generics.ListCreateAPIView):
+    queryset = Message.objects.all().order_by('-created_at')
+    serializer_class = MessageSerializer
