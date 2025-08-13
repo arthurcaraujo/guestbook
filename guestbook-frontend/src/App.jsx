2b6d35e022a38bd0670a725ecb2d396618065906
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-08-13 20:10:58 -0300

    feat: Add form to submit new guestbook messages

--- guestbook-frontend/src/App.jsx
+++ guestbook-frontend/src/App.jsx
@@ -1,28 +1,60 @@
-import './App.css'
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+    const [form, setForm] = useState({name: "", message: ""});
     const [messages, setMessages] = useState([]);
 
+    const fetchMessages = () => {
+        axios.get("http://localhost:8000/messages/")
+            .then(res => setMessages(res.data));
+    }
+
     useEffect(() => {
-    axios.get('http://localhost:8000/api/messages/')
-        .then(res => setMessages(res.data))
-        .catch(err => console.error(err));
+        fetchMessages();
     }, []);
 
+    const handleChange = e => {
+        setForm({...form, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post("http://localhost:8000/messages/", form)
+            .then(() => {
+                setForm({name: "", message: ""});
+                fetchMessages();
+            });
+    };
+
     return (
-    <div>
-        <h1>Guestbook</h1>
-        <ul>
-        {messages.map(msg => (
-            <li key={msg.id}>
-            <strong>{msg.name}</strong>: {msg.message}
-            </li>
-        ))}
-        </ul>
-    </div>
+        <div>
+            <h1>Guestbook</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    name="name"
+                    onChange={handleChange}
+                    placeholder="Your name"
+                    value={form.name}
+                />
+                <textarea
+                    name="message"
+                    onChange={handleChange}
+                    placeholder="Your message"
+                    value={form.message}
+                />
+                <button type="submit">Sign</button>
+            </form>
+            <ul>
+                {messages.map(msg => (
+                    <li key={msg.id}>
+                        <strong>{msg.name}</strong>: {msg.message}
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
-export default App
+export default App;
