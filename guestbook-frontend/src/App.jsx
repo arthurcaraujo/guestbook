857def3d5fa2fda378252450321e30403bda62a7
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-08-12 21:05:37 -0300

    feat: Fetch and display guestbook messages from API

--- guestbook-frontend/src/App.jsx
+++ guestbook-frontend/src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+    axios.get('http://localhost:8000/api/messages/')
+        .then(res => setMessages(res.data))
+        .catch(err => console.error(err));
+    }, []);
+
     return (
-        <div>
-            <h1>Guestbook</h1>
-        </div>
-    )
+    <div>
+        <h1>Guestbook</h1>
+        <ul>
+        {messages.map(msg => (
+            <li key={msg.id}>
+            <strong>{msg.name}</strong>: {msg.message}
+            </li>
+        ))}
+        </ul>
+    </div>
+    );
 }
 
 export default App
